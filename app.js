2ea33865aaf3969bf62ba6f61fe14b520c3e6d6f
shaan-m0 <shaan@m0.ventures>
2024-09-22 17:40:49 +0530

    changed origin

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ const auth_route_1 = __importDefault(require("./routes/auth.route"));
 const task_route_1 = __importDefault(require("./routes/task.route"));
 const app = (0, express_1.default)();
 const PORT = env_1.default.PORT || 5000;
-app.use((0, cors_1.default)({ origin: "https://youseai-frontend-dodyvocy0-shaan71845s-projects.vercel.app/", credentials: true }));
+app.use((0, cors_1.default)({ origin: "https://youseai-frontend.vercel.app/", credentials: true }));
 app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "https://youseai-frontend-dodyvocy0-shaan71845s-projects.vercel.app/");
+    res.setHeader("Access-Control-Allow-Origin", "https://youseai-frontend.vercel.app/");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     res.setHeader("Access-Control-Allow-Credentials", "true");
